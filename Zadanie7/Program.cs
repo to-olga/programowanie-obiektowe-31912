5f2a91c8885b8fbcfe8db51834682e41ac74d5dc
Olga <tomaszewska.olga@hotmail.com>
2025-12-01 18:22:29 +0100

    Zadanie 7

---
+++ Zadanie7/Program.cs
@@ -0,0 +1,22 @@
+﻿// Olga Tomaszewska 31912
+
+// Utwórz tablicę Zwierze[] z obiektami różnych zwierząt i wywołaj DajGlos() w
+// pętli foreach.
+Zwierze[] animals = [new Pies(), new Kot(), new Zwierze(), new Pies()];
+foreach (Zwierze animal in animals)
+{
+    animal.DajGlos();
+}
+
+class Zwierze
+{
+    public virtual void DajGlos() => Console.WriteLine("Zwierzę wydaje dźwięk");
+}
+class Pies : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Hau hau!");
+}
+class Kot : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Miau!");
+}
