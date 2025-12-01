bfba4ad8eaf9661511ab1dd9a66dbe138ff28382
Olga <tomaszewska.olga@hotmail.com>
2025-12-01 17:18:18 +0100

    Zadanie 1

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Olga Tomaszewska 31912
+
+string password;
+do
+{
+    Console.Write("Podaj hasło: ");
+    password = Console.ReadLine();
+}
+while (password != "admin123");
+Console.WriteLine("Zalogowano.");
