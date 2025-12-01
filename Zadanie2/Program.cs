68490f86b7b34a681583fee539ecb42a8ffa765a
Olga <tomaszewska.olga@hotmail.com>
2025-12-01 17:35:09 +0100

    Zadanie 2

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Olga Tomaszewska 31912
+
+double number = 0;
+string numer = "";
+do
+{
+    Console.Write("Podaj liczbę większą od zera: ");
+    numer = Console.ReadLine();
+    double.TryParse(numer, out number);
+}
+while (number <= 0);
+Console.WriteLine("Dobra robota!");
