6b3da808b9259075b04d8fb5fa80c1e3e6236196
Olga <tomaszewska.olga@hotmail.com>
2025-12-01 17:56:34 +0100

    Zadanie 4

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Olga Tomaszewska 31912
+
+// Utwórz klasę Osoba z polami Imie, Wiek i metodą PrzedstawSie().
+// Utwórz kilka obiektów i wywołaj tę metodę.
+
+Osoba olga = new Osoba
+{
+    Imie = "Olga",
+    Wiek = 30
+};
+
+Osoba dexter = new Osoba
+{
+    Imie = "Dexter",
+    Wiek = 5
+};
+
+Osoba maciej = new Osoba
+{
+    Imie = "Maciej",
+    Wiek = 30
+};
+
+olga.PrzedstawSie();
+dexter.PrzedstawSie();
+maciej.PrzedstawSie();
+
+class Osoba
+{
+    public string Imie;
+    public int Wiek;
+    public void PrzedstawSie()
+    {
+        Console.WriteLine($"Cześć, jestem {Imie} i mam {Wiek} lat.");
+    }
+}
